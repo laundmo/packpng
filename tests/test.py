a9319ba81cc134c1693ecf2e169b943f2431539b
laundmo <laurinschmidt2001@gmail.com>
2020-01-22 00:41:15 +0100

    adding simple tests and travis

---
+++ tests/test.py
@@ -0,0 +1,26 @@
+from main import app
+import unittest 
+
+class FlaskTests(unittest.TestCase): 
+
+    def setUp(self):
+        # creates a test client
+        self.app = app.test_client()
+        # propagate the exceptions to the test client
+        self.app.testing = True 
+    
+    def test_home_status_code(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/') 
+
+        # assert the status code of the responses
+        self.assertEqual(result.status_code, 200)
+    
+    def test_faq_status_code(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/faq/') 
+
+        # assert the status code of the response
+        self.assertEqual(result.status_code, 200) 
