fb443009d3bcc396dfe5f69d7f200be4691da495
laundmo <laurinschmidt2001@gmail.com>
2020-01-24 00:48:22 +0100

    change pull_restart to use git stash

--- pull_restart.sh
+++ pull_restart.sh
@@ -1,7 +1,4 @@
-git checkout backup-changes
-git add .
-git commit -m "automatic commit of deployment changes"
-git push -u origin backup-changes
+git stash push
 git fetch --all
 git reset --hard origin/deploy
 systemctl restart packpng.service
