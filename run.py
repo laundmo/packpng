93611ab9ca97f4c4860b3be0723325439fb736de
laundmo <laurinschmidt2001@gmail.com>
2020-01-26 07:24:46 +0100

    removed scripts from run files (that was a bad idea)

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
 from app.app_factory import create_app
 from pathlib import Path
-import data.contributors_to_json
-import app.static.generate_thumbnails
 
 if __name__ == '__main__':
     extra_files = [str(p.resolve()) for p in Path('./').glob('**/*')] # list of all files in the project, so flask knows where to look for changes
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,5 @@
 # for running production WSGI server
 from app.app_factory import create_app
-import data.contributors_to_json
-import app.static.generate_thumbnails
 
 app = create_app()
 
