959769cf351109ea19e7584d0cbf7771dcd667fc
Johann Sebastian Schicho <sebastian-schicho@t-online.de>
2020-07-21 15:30:27 +0200

    added tests, in case this will get merged

--- tests/test.py
+++ tests/test.py
@@ -81,6 +81,14 @@ class FlaskTests(unittest.TestCase):
 
         # assert the status code of the response
         self.assertEqual(result.status_code, 200)
+        
+    def test_panoramasearch_status_code(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/panorama/') 
+
+        # assert the status code of the response
+        self.assertEqual(result.status_code, 200)
 
 
 class ScriptTests(unittest.TestCase): 
